3ea645b68dd5c2dd33f5b0b2100d071e65ecae1c
Pat <stumbaup@oregonstate.edu>
2021-01-02 20:51:26 -0800

    added reveals for rest of page

--- home.js
+++ home.js
@@ -1,7 +1,7 @@
-window.onscroll = function() {myFunction(), myFunction2()};
+window.onscroll = function() {reveal1(), reveal2(), reveal3(), reveal4(), reveal5(), reveal6(), reveal7(), reveal8()};
 
 //if delete, change "mainPicture" class in CSS visability
-function myFunction() {
+function reveal1() {
   if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
     document.getElementById("code").className = "slideUp";
     document.getElementById("myImage").className = "slideUp";
@@ -10,9 +10,44 @@ function myFunction() {
   }
 }
 
-function myFunction2() {
+function reveal2() {
     if (document.body.scrollTop > 450 || document.documentElement.scrollTop > 450) {
-      document.getElementById("resumeTable1").parentNode.className = "slideUp";
-      document.getElementById("resumeTable2").parentNode.className = "slideUp";
+      document.getElementById("revealWorkExperience1").className = "slideUp";
+      document.getElementById("revealWorkExperience2").className = "slideUp";
+    }
+  }
+
+function reveal3() {
+    if (document.body.scrollTop > 875 || document.documentElement.scrollTop > 875) {
+      document.getElementById("hikersGuideReveal").className = "slideUp";
+    }
+  }
+
+  function reveal4() {
+    if (document.body.scrollTop > 1100 || document.documentElement.scrollTop > 1100) {
+      document.getElementById("minesweeperReveal").className = "slideUp";
+    }
+  }
+  function reveal5() {
+    if (document.body.scrollTop > 1500 || document.documentElement.scrollTop > 1500) {
+      document.getElementById("smallShellReveal").className = "slideUp";
+    }
+  }
+
+  function reveal6() {
+    if (document.body.scrollTop > 1850 || document.documentElement.scrollTop > 1850) {
+      document.getElementById("ASMReveal").className = "slideUp";
+    }
+  }
+
+  function reveal7() {
+    if (document.body.scrollTop > 2250 || document.documentElement.scrollTop > 2250) {
+      document.getElementById("mapReveal").className = "slideUp";
+    }
+  }
+
+  function reveal8() {
+    if (document.body.scrollTop > 2550 || document.documentElement.scrollTop > 2550) {
+      document.getElementById("workoutReveal").className = "slideUp";
     }
   }
